5cf89c22ca84c7a98ccf01abf739bb5aca342eef
Guanisnt <allen6010101@gmail.com>
2024-04-27 14:28:52 +0800

    整理code

--- src/Time.java
+++ src/Time.java
@@ -6,17 +6,18 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 
 public class Time extends JLabel {
-    JLabel lab1 = new JLabel("60秒");
+    JLabel lab1z=new JLabel("60秒");
     window window;
-    int sec = 60;
+    int sec=60;
 
-    Time(window w) {
+    Time(window w){
         this.window = w;
     }
 
-    public void paint(Graphics2D g) {
-        g.setColor(new Color(0,0,0));  // 畫筆顏色
-        g.setFont(new Font("Verdana", Font.BOLD, 20));  // 字型
+    public void paint(Graphics2D g){
+        g.setColor(new Color(0,0,0)); //畫筆顏色
+        g.setFont(new Font("Verdana", Font.BOLD, 20)); //字型
         g.drawString(String.valueOf(sec), 286, 70);
     }
+
 }
